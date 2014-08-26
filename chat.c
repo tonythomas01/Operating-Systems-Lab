b9b6bf2aa200fe4393a276ac4c3e21ab74f87821
Tony Thomas <01tonythomas@gmail.com>
2014-08-26 22:27:49 +0530

    Modified chat program

--- chat.c
+++ chat.c
@@ -1,44 +1,41 @@
 #include<stdio.h>
-#include<unistd.h> 
 #include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
 #include<string.h>
 
 int main() {
-	int fd[2], fdn[2], n;
-	pid_t childpid; 
-	pipe( fd ); 
-	pipe( fdn );
-	char input[50];
-	char quit[10] = "quit";
-	char inputParent[50];
-	char buffer[50];
-	char bufferp[50];
-	childpid = fork();
-	if( childpid == -1) { 
-		perror("fork");
-		exit(1); 
-	}
-	while( 1 ) {
-		if (childpid == 0) {
-			close( fd[0] );
-			
-			scanf( "%s", &input );
-			if( strcmp( quit, input ) == 0 ) {
-				exit(1);
-			}
-			write( fd[1], input, (strlen(input)+1) );
+    int fd[2];
+    int fdtwo[2];
+    pid_t childid;
 
-			n  =  read(fdn[0], bufferp, sizeof( bufferp ) );
-			printf("Child says : %s\n", bufferp );
-			
-		} else {
-			close( fd[1] );
-			n = read(fd[0], buffer, sizeof( buffer ) );
-			printf("Received string: %s\n", buffer);
-			
-			scanf( "%s", &inputParent );
-			write( fdn[1], inputParent, (strlen(inputParent)+1) );
-			sleep(1);
-		}
-	}
+    pipe( fdtwo );
+    pipe(fd);
+
+    char writeString[10];
+    char childString[10];
+
+    childid = fork();
+    while(1) {
+        if( childid == 0 ) {
+            //child
+            close( fd[1] );
+            read( fd[0], writeString, 10);
+            printf( "Child: The recieved string is %s", writeString );
+
+            printf(" \n Child > " );
+            scanf( "%s", &childString );
+            write( fdtwo[1], childString, 10 );
+
+        } else {
+            //parent
+            close( fd[0] );
+            printf( "\n Parent> ");
+            scanf( "%s" , &writeString );
+            write( fd[1], writeString, 10);
+
+            read( fdtwo[0], childString, 10 );
+            printf( "Parent: recieved  string is %s", childString );
+        }
+    }
 }
