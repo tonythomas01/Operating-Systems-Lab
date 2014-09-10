bbdab539f804b8af9fb913652db599c6940b5c50
Tony Thomas <01tonythomas@gmail.com>
2014-09-10 19:16:35 +0530

    Added the file copy program using Pipe and fork

---
+++ fileCopyFork.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<string.h>
+#include<stdlib.h>
+
+int main( int argc, char* argv[] ) {
+	int fdone[2];
+	pid_t childid;
+
+	char readBuff[50];
+	char writeBuff[50];
+	int readCounter;
+
+	pipe( fdone );
+
+	int fileOpen = open( argv[1], 0 );
+	int targetFile = open( argv[2], 0666 );
+	
+	if ( fileOpen == -1 || targetFile == -1 ) {
+	    printf( "Opening file failed " );
+	    exit(1);
+	}
+	childid = fork();
+
+	if( childid == 0 ) {
+	    // inside the child prcocess
+	    close( fdone[1] );
+
+	    read( fdone[0], readBuff, sizeof( readBuff ) );
+	    printf( "The recived string is : %s", readBuff );
+
+	    //Writing to the target fileOpen
+	    write( targetFile, readBuff, sizeof( readBuff ) + 1 );
+	} else {
+	    // inside the parent process
+	    close( fdone[0] );
+	    // code to read from a text file
+
+	    while( (readCounter = read( fileOpen, readBuff, sizeof( readBuff ) ) > 0 ) )  {
+		write( fdone[1], readBuff, sizeof( readBuff ) );
+	    }
+	    close( fdone[1] );
+	}
+}
