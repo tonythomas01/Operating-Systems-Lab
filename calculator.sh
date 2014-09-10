007596bd16f92cfca246d3bcb9d7000bc7ef4b2d
Tony Thomas <01tonythomas@gmail.com>
2014-09-10 19:40:16 +0530

    Added the calculator program

---
+++ calculator.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "First Opearnd "
+read x
+echo "Operator "
+read operator
+echo "Third Opearnd "
+read y
+case $operator in
++) echo "add"
+echo $(( x + y ))	;;
+-) echo "sub"
+echo $(( x - y )) 	;;
+*) echo "mult"
+echo $(( x * y ))	;;
+/) echo "div"
+echo $(( x / y ))	;;
+esac
